42724943cb6a1e57d75875278c6e0a9a25ab12e4
David Roe <roed.math@gmail.com>
2026-08-10 16:48:06 -0400

    Remove undeclared cohomology supplier from local-fields branch

--- TauCetiRoadmap.lean
+++ TauCetiRoadmap.lean
@@ -27,5 +27,4 @@ import TauCetiRoadmap.ContourIntegration.Suggested
 import TauCetiRoadmap.ConformalMapping.Suggested
 import TauCetiRoadmap.OrthogonalL2Bases.Suggested
 import TauCetiRoadmap.OptimalTransport.Suggested
-import TauCetiRoadmap.ProfiniteCohomology.Suggested
 import TauCetiRoadmap.LocalFieldsRamification.Suggested
